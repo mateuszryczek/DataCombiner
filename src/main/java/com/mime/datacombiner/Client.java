04758531c6fd31448aac2c487ccb0169ca5e4027
Mateusz Ryczek <mateuszryczek@yahoo.com>
2020-11-14 17:08:33 +0100

    add log4j

--- src/main/java/com/mime/datacombiner/Client.java
+++ src/main/java/com/mime/datacombiner/Client.java
@@ -1,10 +1,16 @@
 package com.mime.datacombiner;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class Client {
+
+    private static final Logger LOGGER = Logger.getLogger(Client.class);
 
     public static void main(String[] args) {
         DataCombiner<Long> combiner = new DataCombiner<>(new RandomLongDataProvider(), new RestLongDataProvider(), new PlusOperation());
-        System.out.println(combiner.combine());
+        BasicConfigurator.configure();
+        LOGGER.info(combiner.combine());
     }
 
 }
