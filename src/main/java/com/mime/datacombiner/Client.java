9b9c25089acf9ca7f52f052ace9d74adf3702007
Mateusz Ryczek <mateuszryczek@yahoo.com>
2020-11-14 17:55:13 +0100

    update log4j configuration

--- src/main/java/com/mime/datacombiner/Client.java
+++ src/main/java/com/mime/datacombiner/Client.java
@@ -1,15 +1,14 @@
 package com.mime.datacombiner;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Client {
 
-    private static final Logger LOGGER = Logger.getLogger(Client.class);
+    private static final Logger LOGGER = LogManager.getLogger(Client.class);
 
     public static void main(String[] args) {
         DataCombiner<Long> combiner = new DataCombiner<>(new RandomLongDataProvider(), new RestLongDataProvider(), new PlusOperation());
-        BasicConfigurator.configure();
         LOGGER.info(combiner.combine());
     }
 
